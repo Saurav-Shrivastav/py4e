1c90aad9d1d5bb91c480606cdd9a4fe76a192321
Saurav <sauravsrivastav103@gmail.com>
2019-10-29 14:26:24 +0530

    random commit

--- for_loop.py
+++
@@ -1,8 +0,0 @@
-print("before")
-count = 0
-sum = 0
-for i in [9,12,41,15,74,6]:
-    count = count + 1
-    sum = sum + i
-    print(count, sum, i)
-print("after")
--- largest.py
+++
@@ -1,9 +0,0 @@
-largest_so_far = 0
-print("before", largest_so_far)
-for i in [9,41,12,74,15]:
-    if largest_so_far==0 :
-        largest_so_far= i
-        continue
-    if largest_so_far<i :
-        largest_so_far = i
-print("after", largest_so_far)
